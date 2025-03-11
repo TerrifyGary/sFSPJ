28bbc94b69fd0761992d7aa89184e89403d99933
TerrifyGary <yoyoman1415926@gmail.com>
2025-03-11 13:46:41 +0800

    Complete docker Setting

--- WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4db701445cee1a4c0531477282761d2270a5307d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ea2dfde1c384c8bf6e3215b1bd8ca8367c00d03")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
